072980d4598ccc56ea62c61e0ba1cac68b91f9bf
NexflareNEU <agarwal.hars@northeastern.edu>
2022-09-21 17:16:29 -0400

    Linting :bug: fix

--- src/main/java/edu/neu/csye6200/Cart.java
+++ src/main/java/edu/neu/csye6200/Cart.java
@@ -3,50 +3,50 @@ package edu.neu.csye6200;
 import java.text.DecimalFormat;
 
 public class Cart {
-	private double myCash;
-	private double myTotal;
-	private double myChange;
-	private static final DecimalFormat df = new DecimalFormat("0.00");
-	
-	Cart(int cash) {
-		this.myCash = cash;
-		this.myChange = cash;
-	}
-	
-	public void sillyCheckout(double cash, double price, double total, double change) {
-		this.myTotal = total + price;
-		this.myChange = change - price;
-	}
-	
-	public double getMyCash() {
-		return myCash;
-	}
+  private double myCash;
+  private double myTotal;
+  private double myChange;
+  private static final DecimalFormat df = new DecimalFormat("0.00");
 
-	public double getMyTotal() {
-		return myTotal;
-	}
+  Cart(int cash) {
+    this.myCash = cash;
+    this.myChange = cash;
+  }
 
-	public double getMyChange() {
-		return myChange;
-	}
+  public void sillyCheckout(double cash, double price, double total, double change) {
+    this.myTotal = total + price;
+    this.myChange = change - price;
+  }
 
-	public double checkout(Item myItem) {
-		this.myTotal+= myItem.getPrice();
-		this.myChange-=myItem.getPrice();
-		return this.myChange;
-	}
-	
-	public double checkout(Cart myCart,Item myItem) {
-		myCart.myChange-=myItem.getPrice();
-		myCart.myTotal+=myItem.getPrice();
-		return myCart.myChange;
-	}
+  public double getMyCash() {
+    return myCash;
+  }
 
-	public String toString() {
-		StringBuilder str = new StringBuilder("");
-		str.append("Cash given=" + String.valueOf(df.format(myCash))+"\n");
-		str.append("Total Cost= "+String.valueOf(df.format(this.myTotal))+"\n");
-		str.append("Change =" + String.valueOf(df.format(this.myChange)));
-		return str.toString();
-	}
+  public double getMyTotal() {
+    return myTotal;
+  }
+
+  public double getMyChange() {
+    return myChange;
+  }
+
+  public double checkout(Item myItem) {
+    this.myTotal += myItem.getPrice();
+    this.myChange -= myItem.getPrice();
+    return this.myChange;
+  }
+
+  public double checkout(Cart myCart, Item myItem) {
+    myCart.myChange -= myItem.getPrice();
+    myCart.myTotal += myItem.getPrice();
+    return myCart.myChange;
+  }
+
+  public String toString() {
+    StringBuilder str = new StringBuilder("");
+    str.append("Cash given = " + String.valueOf(df.format(myCash)) + "\n");
+    str.append("Total Cost = " + String.valueOf(df.format(this.myTotal)) + "\n");
+    str.append("Change = " + String.valueOf(df.format(this.myChange)));
+    return str.toString();
+  }
 }
--- src/main/java/edu/neu/csye6200/Driver.java
+++ src/main/java/edu/neu/csye6200/Driver.java
@@ -4,12 +4,12 @@ package edu.neu.csye6200;
  * @author SaiAkhil
  */
 public class Driver {
-    public static void main(String[] args) {
-        System.out.println("============Main Execution Start===================\n\n");
+  public static void main(String[] args) {
+    System.out.println("============Main Execution Start===================\n\n");
 
-        //Add your code in between these two print statements
-        Item.demo();
+    // Add your code in between these two print statements
+    Item.demo();
 
-        System.out.println("\n\n============Main Execution End===================");
-    }
+    System.out.println("\n\n============Main Execution End===================");
+  }
 }
